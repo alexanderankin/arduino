24d29014c69b67b1dbb6e7c2601a0901ed880553
David Ankin <david.ankin@stonybrook.edu>
2015-05-19 19:13:54 -0400

    Made some skeleton code

--- helloworld_ac/helloworld_ac.ino
+++ helloworld_ac/helloworld_ac.ino
@@ -1,8 +1,43 @@
-void setup(){
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(9600); 
 }
-void loop(){
-  Serial.println("hw");
-  int a = 0;
-  for(;;) {a++;}
+
+double error;
+
+#define al 60
+
+void loop() {
+  int iters = 60;
+  
+  double a = 0.0;
+  double inc = 2 * PI / 60;
+  
+  double gooddata[al];
+  double baddata[al];
+  double fixeddata[al];
+  for (int i = 0; i < iters; i++) {
+    if (i % 5 != 0) continue;
+    gooddata[i] = generate(a, i, inc);
+    baddata[i] = worsen(gooddata[i]); // 5% error
+    fixeddata[i] = algorithm(baddata[i]);
+    Serial.print(fixeddata[i] - gooddata[i]);
+    Serial.print("\n");
+  }
+  
+  Serial.print("done");
+  delay(100000);
+
+}
+
+double generate (double a, int i, double inc) {
+  return sin(a + (i * inc));
+}
+
+double worsen(double a) {
+  double m_error = (random(-1,1) * .05);
+  error = m_error;
+  return a + m_error;
+}
+double algorithm(double a) {
+  return a - error;
 }
